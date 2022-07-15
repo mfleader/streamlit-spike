8d23914d7e495f28346577136b9a315448b3defb
mleader <mleader@redhat.com>
2022-07-15 14:16:26 -0400

    write logic to determine if cpus and memory are being over used

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ import config
 import plotly.express as px
 
 from scipy.stats import lognorm, norm, invgauss, invgamma, gamma
-# import scipy.stats as scistats
+
 
 import statistics as stats
 from math import log, exp
@@ -28,13 +28,6 @@ _ = lang_translations.gettext
 #         </style>
 #         """
 # st.markdown(hide_menu_style, unsafe_allow_html=True)
-
-# import cProfile, pstats
-
-# import gevent
-# profiler = cProfile.Profile()
-
-# profiler.enable()
 
 
 # @st.experimental_singleton
@@ -121,15 +114,7 @@ def simulated_draws(sample: pd.Series, n_draws: int = 10_000):
     )
 
 
-
-
 def model_data_world(df: pd.DataFrame, kpi: str):
-    # simdf = simulated_draws(df[kpi])
-    # print(simdf.describe())
-    # print(np.mean(simdf))
-    # print(np.median(simdf))
-    # print(np.std(simdf))
-    # print(np.max(simdf))
     return pd.concat((
         df[['uuid', kpi]],
         pd.DataFrame.from_records(
@@ -144,37 +129,6 @@ def main():
     )
 
     st.title(_("DASHBOARD_TITLE"))
-
-    # data_col, cluster_col = st.columns(2)
-
-    # with data_col:
-    #     st.subheader(_("DATA_SOURCES_TITLE"))
-    #     selected_datasource = st.radio(_("SELECT_DATASOURCE"), ("PostgreSQL DB", "CSV"))
-
-    #     if selected_datasource == "PostgreSQL DB":
-    #         engine = get_engine()
-    #         session = next(get_session(engine))
-
-    #         job_uuids = session.exec(
-    #             select(Run_Metrics.uuid)
-    #         ).all()
-    #         data = session.exec(
-    #             select(Run_Metrics)
-    #         ).all()
-    #         df_og = pd.DataFrame.from_records(
-    #             (d.dict() for d in data)
-    #         )
-    #     elif selected_datasource == "CSV":
-    #         df_og = pd.read_csv(
-    #             'data/run_metrics_2022-07-08.csv'
-    #         )
-    #     job_uuids = df_og['uuid'].values.tolist()
-
-    #         # with job_uuid_select:
-    #     job_selection = st.selectbox(
-    #         _("SELECT_UUID"),
-    #         options=job_uuids,
-    #     )
 
     data_src_sidebar_container = st.sidebar.container()
 
@@ -201,15 +155,10 @@ def main():
             )
         job_uuids = df_og['uuid'].values.tolist()
 
-
         job_selection = st.selectbox(
             _("SELECT_UUID"),
             options=job_uuids,
         )
-
-
-    # st.markdown("""---""")
-    # job_uuid_select = st.columns(1)
 
     cluster_selection_df = df_og.loc[df_og['uuid'] == job_selection]
 
@@ -223,27 +172,6 @@ def main():
     # use unmatched cluster data when your matched cluster sample is too small
     if similar_clusters.shape[0] < 3:
         similar_clusters = df_og
-
-
-    # with cluster_col:
-    #     st.subheader(_("CLUSTER_INFO_TITLE"))
-    #     st.metric(
-    #         label = _("OPENSHIFT_VERSION_METRIC"),
-    #         value = cluster_selection_df['ocp_version'].values[0]
-    #     )
-    #     st.metric(
-    #         label = _("CNI_METRIC"),
-    #         value = cluster_selection_df['sdn_type'].values[0]
-    #     )
-    #     st.metric(
-    #         label = _("PLATFORM_METRIC"),
-    #         value = cluster_selection_df['platform'].values[0]
-    #     )
-    #     st.metric(
-    #         # label = _("NODE_SIZE"),
-    #         label = 'Node Size',
-    #         value = cluster_selection_df['worker_nodes_type'].values[0]
-    #     )
 
     cluster_side_container = st.sidebar.container()
 
@@ -267,41 +195,107 @@ def main():
         )
 
 
-        # st.metric(
-        #     label = 'Data Collection Date',
-        #     value = str(pd.Timestamp.fromtimestamp(int(df_og[df_og['uuid'] == job_selection]['timestamp'].values[0] / 1_000_000),'UTC'))
-        # )
-
-
-
-
     # ========================================================= #
     # Different instance section
     # ========================================================= #
     st.header(_("DIFF_INSTANCE_Q_TITLE"))
+    control_nodes_col, worker_nodes_col,  = st.columns(2)
+
+    ec2_instance_data = pd.DataFrame(
+        data = [
+            {
+                'platform': 'ROSA',
+                'instance_type': 'm5.4xlarge',
+                'vcpu': 16,
+                'memory': 64,
+            },
+            {
+                'platform': 'ROSA',
+                'instance_type': 'm5.2xlarge',
+                'vcpu': 8,
+                'memory': 32,
+            },
+            {
+                'platform': 'AWS',
+                'instance_type': 'm5.2xlarge',
+                'vcpu': 8,
+                'memory': 32,
+            },
+            {
+                'platform': 'AWS',
+                'instance_type': 'r5.4xlarge',
+                'vcpu': 16,
+                'memory': 128
+            }
+        ]
+    )
 
 
-    worker_cpu_col, control_cpu_col = st.columns(2)
 
-    control_cpu = similar_clusters[similar_clusters['uuid'] == job_selection]['nodecpu_masters_avg'].values[0]
-    control_cpu_agg = similar_clusters['nodecpu_masters_avg'].mean()
-    worker_cpu = similar_clusters[similar_clusters['uuid'] == job_selection]['nodecpu_workers_avg'].values[0]
-    worker_cpu_agg = similar_clusters['nodecpu_workers_avg'].mean()
+    cluster = similar_clusters[similar_clusters['uuid'] == job_selection]
 
-    with control_cpu_col:
+    control_cpu = cluster['nodecpu_masters_avg'].values[0]
+    control_mem = cluster['nodememoryutilization_workers_avg'].values[0] / 1073741824
+    worker_cpu = cluster['nodecpu_workers_avg'].values[0]
+    worker_mem = cluster['nodememoryutilization_workers_avg'].values[0] / 1073741824
+
+
+
+    # print(df2.columns)
+
+    # print(df2[['uuid', 'platform', 'instance_type', 'vcpu', 'memory']])
+
+    # control_cpu_agg = similar_clusters['nodecpu_masters_avg'].mean()
+
+    # worker_cpu_agg = similar_clusters['nodecpu_workers_avg'].mean()
+    control_cpu_delta = control_cpu - ec2_instance_data.loc[
+            ( ec2_instance_data['platform'] == cluster['platform'].values[0] ) &
+            ( ec2_instance_data['instance_type'] == cluster['master_nodes_type'].values[0] )
+        ]['vcpu'].values[0]
+
+    # print(control_cpu_delta)
+
+    control_mem_delta = control_mem - .85 * ec2_instance_data.loc[
+            ( ec2_instance_data['platform'] == cluster['platform'].values[0] ) &
+            ( ec2_instance_data['instance_type'] == cluster['worker_nodes_type'].values[0] )
+        ]['memory'].values[0]
+
+    worker_cpu_delta = worker_cpu - .85 * ec2_instance_data.loc[
+            ( ec2_instance_data['platform'] == cluster['platform'].values[0] ) &
+            ( ec2_instance_data['instance_type'] == cluster['worker_nodes_type'].values[0] )
+        ]['vcpu'].values[0]
+
+    worker_mem_delta = worker_mem - .85 * ec2_instance_data.loc[
+            ( ec2_instance_data['platform'] == cluster['platform'].values[0] ) &
+            ( ec2_instance_data['instance_type'] == cluster['worker_nodes_type'].values[0] )
+        ]['memory'].values[0]
+
+    with control_nodes_col:
         st.metric(
             label = _("CONTROL_NODE_CPU"),
             value = round(control_cpu,2),
-            delta = round(control_cpu - control_cpu_agg, 2),
+            delta = round(control_cpu_delta, 2),
             delta_color = 'inverse',
         )
+        st.metric(
+            label = _("CONTROL_NODE_MEM"),
+            value = str(round(control_mem, 2)) + ' GiB',
+            delta = round(control_mem_delta, 2),
+            delta_color = 'inverse'
+        )
 
-    with worker_cpu_col:
+    with worker_nodes_col:
         st.metric(
             label = _("WORKER_NODE_CPU"),
             value = round(worker_cpu,2),
-            delta = round(worker_cpu - worker_cpu_agg,2),
+            delta = round(worker_cpu_delta,2),
             delta_color = 'inverse',
+        )
+        st.metric(
+            label = _("WORKER_NODE_MEM"),
+            value = str(round(worker_mem, 2)) + ' GiB',
+            delta = round(worker_mem_delta, 2),
+            delta_color = 'inverse'
         )
 
     # ========================================================= #
@@ -329,7 +323,7 @@ def main():
     with latency_col_2:
         st.metric(
             label = _("POD_LATENCY"),
-            value = round(pod_latency, 2),
+            value = str(round(pod_latency, 2)) + ' ms',
             delta = round(pod_latency - pod_start_ltcy_grade_scale.great_hi, 2),
             delta_color = 'inverse',
             help = _("POD_LATENCY_EXPLANATION")
@@ -369,17 +363,6 @@ def main():
     'p99thetcddiskwalfsyncdurationseconds_avg',
     'etcdleaderchangesrate_max']].apply(lambda x: x <= x.quantile(q=.5)).astype(int).apply(pd.DataFrame.sum, axis=1)
 
-    # etcdf['health_score_pct'] = etcdf['health_score'] / 4
-
-    # p_etcd = (
-    #     p9.ggplot(etcdf[etcdf['uuid'] == job_selection], p9.aes(x='uuid', y='health_score_pct')) +
-    #     p9.geom_col(p9.aes(fill = 'health_score_pct')) +
-    #     p9.scale_fill_gradient2(low = 'red', mid = 'yellow', high = 'green') +
-    #     p9.ylim(0,1) +
-    #     p9.coord_flip()
-    # )
-
-    # st.plotly_chart(p_etcd)
 
     etcd_health_grade_scale = config.get_thresholds("", "", "etcd_health", etcdf['health_score'])
 
@@ -406,11 +389,6 @@ def main():
             delta_color = 'normal'
         )
 
-
-
-    # print(similar_clusters[[ 'workload', 'uuid', 'p99thetcddiskwalfsyncdurationseconds_avg']])
-    # print(similar_clusters[[ 'workload', 'uuid', 'etcdleaderchangesrate_max']])
-    # print('=============================================')
 
 
     with st.expander(_("ETCD_HEALTH_ADVANCED")):
@@ -455,130 +433,6 @@ def main():
 
 
 
-    # has_job_ids = st.radio(
-    #     'Do you know the workload job UUIDs?',
-    #     ['yes', 'no']
-    # )
-
-    # if has_job_ids == 'yes':
-    # col_a, col_b = st.columns(2)
-
-    # with col_a:
-    #     job_a = st.selectbox(
-    #         'Group A Job UUID',
-    #         options=job_uuids,
-    #     )
-
-    # with col_b:
-    #     job_b = st.multiselect(
-    #         'Group B Job UUID',
-    #         options=job_uuids,
-    #     )
-
-    # elif has_job_ids == 'only one':
-
-    #     job_uuids = list(df['uuid'])
-
-    #     job_a = st.selectbox(
-    #         'Group A Job UUID',
-    #         options=job_uuids,
-    #     )
-
-    #     platforms = list(df['platform'].unique())
-    #     platform_selections = st.multiselect(
-    #         'Cloud Platform',
-    #         options=platforms,
-    #         default=platforms
-    #     )
-    #     df = df[df['platform'].isin(platform_selections)]
-    #     grid = ix_table.main(
-    #         df[df['uuid'] == job_a]
-    #     )
-    # elif has_job_ids == 'no':
-    #     st.write("Go ahead, click on a row in the table below!")
-    #     grid = ix_table.main(df_og)
-    #     df = grid['data']
-
-    # if grid:
-    #     st.write("You selected:")
-    #     st.json(grid["selected_rows"])
-
-
-
-
-
-    # st.markdown("Quantitative Statistics")
-    # st.sidebar.subheader("Graph Settings")
-    # chart_choice = st.sidebar.radio("",["QQplot","Boxplot","Histogram"])
-    # # df = grid['data']
-    # p = p9.ggplot(df)
-    # top = st.columns((1,1))
-    # bottom = st.columns(1)
-
-    # global numeric_columns
-    # global non_numeric_columns
-    # try:
-    #     numeric_columns = list(df.select_dtypes(['float','int']).columns)
-    #     non_numeric_columns = list(df.select_dtypes(['object']).columns)
-    #     non_numeric_columns.append(None)
-    # except Exception as e:
-    #     print(e)
-
-
-    # if chart_choice == "QQplot":
-    #     with top[1]:
-    #         x = st.selectbox('X-Axis', options=numeric_columns)
-    #         cv = st.selectbox('Color', options=non_numeric_columns)
-    #     if cv != None:
-    #         p = (
-    #             p +
-    #             p9.stat_qq(p9.aes(sample=x, color=cv)) +
-    #             p9.stat_qq_line(p9.aes(sample=x, color=cv)) +
-    #             p9.labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
-    #         )
-    #     else:
-    #        p = (
-    #             p +
-    #             p9.stat_qq(p9.aes(sample=x)) +
-    #             p9.stat_qq_line(p9.aes(sample=x)) +
-    #             p9.labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
-    #         )
-
-    # if chart_choice == 'Boxplot':
-    #     with top[1]:
-    #         x = st.selectbox('X-Axis', options=numeric_columns)
-    #         cv = st.selectbox("Color", options=non_numeric_columns)
-    #     if cv != None:
-    #         p = p + p9.geom_boxplot(p9.aes(x=cv,y=x, fill = cv)) + p9.coord_flip()
-    #     else:
-    #         p = p + p9.geom_boxplot(p9.aes(x=1,y=x,width=.1),color="darkblue", fill="lightblue") + p9.coord_flip()
-
-    # if chart_choice == 'Histogram':
-    #     with top[1]:
-    #         x = st.selectbox('X-Axis', options=numeric_columns)
-    #         cv = st.selectbox("Color", options=non_numeric_columns)
-    #         bins = st.slider("Number of Bins", min_value=1,max_value=40, value=7)
-    #     if cv != None:
-    #         p = p + p9.geom_histogram(p9.aes(x=x, fill = cv, color = cv),position= "identity",alpha=.4, bins = bins)
-    #     else:
-    #         p = p + p9.geom_histogram(p9.aes(x=x),color="darkblue", fill="lightblue", bins = bins)
-
-
-    # with top[1]:
-    #     st.plotly_chart(p)
-
-    # with top[0]:
-    #     st.write(df[[cv, x]])
-
-    # with bottom[0]:
-    #     st.write(df.describe().T)
-    #     if cv != None:
-    #         st.write(df.groupby([cv]).describe())
-
-
-    # with st.expander("Advanced Data Access"):
-    #     st.write("Go ahead, click on a row in the table below!")
-    #     ix_table.main(df)
 
 
 
